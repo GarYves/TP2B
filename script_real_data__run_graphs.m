aa01a021c12022c1271831b715fb87872417e17f
Yves Garant <ygarant@drw.com>
2021-11-29 16:32:49 -0500

    more graphs

--- script_real_data__run_graphs.m
+++ script_real_data__run_graphs.m
@@ -10,7 +10,7 @@ subplot(3,1,1)
 plot(spx_ret,'k')
 xlabel('days')
 ylabel('log-returns')
-title('Simulated returns')
+title('S&P 500 daily log-returns')
 xlim([0, nb_days])
 
 subplot(3,1,2)
@@ -18,7 +18,7 @@ hold on
 plot(V_ret_spx_est(:,1),'Color','b')
 plot(V_ret_spx_est(:,2),'Color' ,'r','LineStyle','--')
 plot(V_ret_spx_est(:,3),'Color' ,'r','LineStyle','--')
-legend('Filtered variance','2.5%','97.5%','Location','northwest')
+legend('Filtered variance ','2.5%','97.5%','Location','northwest')
 title('Volatility filtered using returns and the estimated parameters')
 xlim([0, nb_days])
 
@@ -27,7 +27,7 @@ hold on
 plot((252)*spx_RV(:,1),'Color' ,'r')
 plot(V_ret_spx_est(:,1),'Color','b')
 legend('Calculated realized variance ','Filtered Variance estimated parameters','Location','northwest')
-title('Volatility filtered using estimated parameters vs Calculated Realized Variance')
+title('Volatility filtered using using returns vs Calculated Realized Variance')
 xlim([0, nb_days])
 
 
@@ -38,7 +38,7 @@ subplot(3,1,1)
 plot(spx_ret,'k')
 xlabel('days')
 ylabel('log-returns')
-title('Simulated returns')
+title('S&P 500 daily log-returns')
 xlim([0, nb_days])
 
 subplot(3,1,2)
@@ -55,6 +55,109 @@ hold on
 plot((252)*spx_RV(:,1),'Color' ,'r')
 plot(V_RV_spx_est(:,1),'Color','b')
 legend('Calculated realized variance ','Filtered Variance estimated parameters','Location','northwest')
+title('Volatility filtered uusing returns+RV vs Calculated Realized Variance')
+xlim([0, nb_days])
+
+
+%% Graph of comparison between variances
+figure
+
+subplot(3,1,1)
+hold on
+plot(252*spx_RV(:,1),'Color' ,'r', 'LineWidth', 0.1)
+plot(V_ret_spx_est(:,1),'Color','k','LineWidth', 2)
+legend('Calculated realized variance','Filtered Variance with returns', 'Location','northwest')
 title('Volatility filtered using estimated parameters vs Calculated Realized Variance')
 xlim([0, nb_days])
+ylim([0,0.1])
+hold off
+
+
+subplot(3,1,2)
+hold on
+plot(252*spx_RV(:,1),'Color' ,'r', 'LineWidth', 0.1)
+plot(V_RV_spx_est(:,1),'Color','b','LineWidth', 2)
+legend('Calculated realized variance', 'Filtered Variance with returns+RV','Location','northwest')
+title('Volatility filtered using estimated parameters vs Calculated Realized Variance')
+xlim([0, nb_days])
+ylim([0,0.1])
+hold off
+
+
+subplot(3,1,3)
+hold on
+plot(V_ret_spx_est(:,1) - V_RV_spx_est(:,1),'Color',[0,0,0.5])
+legend('difference between ret and ret+rv','Location','northwest')
+xlim([0, nb_days])
+hold off
+
+
+%% Graph of comparison between the confidence intervals
+
+
+
+
+%% Graph shape of the log-likelihood function for returns only
+figure
+ 
+
+ymin = min([L_ret_mu; L_ret_kappa; L_ret_theta; L_ret_sigma; L_ret_rho]);
+ymax = max([L_ret_mu; L_ret_kappa; L_ret_theta; L_ret_sigma; L_ret_rho]);
+ylim_ = [ymin, ymax];
+
+subplot(3,2,1)
+[ret_mu2,idx]=sort(ret_mu);
+plot(ret_mu2,L_ret_mu(idx),'k')
+hold on
+plot(ret_mu(end),L_ret_mu(end),'b*')
+ylabel('Log-Likelihood')
+xlabel('\mu')
+legend('Log-Likelihood','Optimized parameter')
+title('Parameter \mu')
+ylim(ylim_)
+
+subplot(3,2,2)
+[ret_kappa2,idx]=sort(ret_kappa);
+plot(ret_kappa2,L_ret_kappa(idx),'k')
+hold on
+plot(ret_kappa(end),L_ret_kappa(end),'b*')
+ylabel('Log-Likelihood')
+xlabel('\kappa')
+legend('Log-Likelihood','Optimized parameter')
+title('Parameter \kappa')
+ylim(ylim_)
+
+subplot(3,2,3)
+[ret_theta2,idx]=sort(ret_theta);
+plot(ret_theta2,L_ret_theta(idx),'k')
+hold on
+plot(ret_theta(end),L_ret_theta(end),'b*')
+ylabel('Log-Likelihood')
+xlabel('\theta')
+legend('Log-Likelihood','Optimized parameter')
+title('Parameter \theta')
+ylim(ylim_)
+
+subplot(3,2,4)
+[ret_sigma2,idx]=sort(ret_sigma);
+plot(ret_sigma2,L_ret_sigma(idx),'k')
+hold on
+plot(ret_sigma(end),L_ret_sigma(end),'b*')
+ylabel('Log-Likelihood')
+legend('Log-Likelihood','Optimized parameter')
+title('Parameter \sigma')
+ylim(ylim_)
+
+subplot(3,2,5)
+[ret_rho2,idx]=sort(ret_rho);
+plot(ret_rho2,L_ret_rho(idx),'k')
+hold on
+plot(ret_rho(end),L_ret_rho(end),'b*')
+ylabel('Log-Likelihood')
+xlabel('\rho')
+legend('Log-Likelihood','Optimized parameter')
+title('Parameter \rho')
+ylim(ylim_)
+
+clear idx ret_mu2 ret_kappa2 ret_theta2 ret_sigma2 ret_rho2 ylim_
 
---
+++ script_real_data__run_likelihood_functions.m
@@ -0,0 +1,95 @@
+save_results = true;
+
+% returns only
+
+n_steps = 40;
+ret_mu = [linspace(-0.05, 0.20, n_steps), mu_ret_est];
+ret_kappa = [linspace(5, 15, n_steps), kappa_ret_est];
+ret_theta = [linspace(0.05^2, 0.15^2, n_steps), theta_ret_est];
+ret_sigma = [linspace(0.3, 0.8, n_steps), sigma_ret_est];
+ret_rho = [linspace(-0.8, -0.1, n_steps), rho_ret_est];
+
+L_ret_mu = zeros(length(ret_mu),1);
+L_ret_kappa = zeros(length(ret_kappa),1);
+L_ret_theta = zeros(length(ret_theta),1);
+L_ret_sigma = zeros(length(ret_sigma),1);
+L_ret_rho = zeros(length(ret_rho),1);
+
+parfor i=1:length(ret_mu)
+    x1 = x_ret_spx_est(1:5);
+    x1(1) = ret_mu(i);
+    L_ret_mu(i,1) = Heston93_nat(x1, simul_real, spx_ret, 1);
+    
+    x1 = x_ret_spx_est(1:5);
+    x1(3) = ret_theta(i);
+    L_ret_theta(i,1) = Heston93_nat(x1, simul_real, spx_ret, 1);
+
+    i
+end
+
+parfor i=1:length(ret_mu)
+    x1 = x_ret_spx_est(1:5);
+    x1(2) = ret_kappa(i);
+    L_ret_kappa(i,1) = Heston93_nat(x1, simul_real, spx_ret, 1);
+
+    x1 = x_ret_spx_est(1:5);
+    x1(4) = ret_sigma(i);
+    L_ret_sigma(i,1) = Heston93_nat(x1, simul_real, spx_ret, 1);
+    
+    i
+end
+
+parfor i=1:length(ret_mu)
+    x1 = x_ret_spx_est(1:5);
+    x1(5) = ret_rho(i);
+    L_ret_rho(i,1) = Heston93_nat(x1, simul_real, spx_ret, 1);
+
+    i
+end
+
+clear x1 i n_steps
+
+% returns + RV
+
+n_steps = 40;
+RV_mu = [linspace(-0.05, 0.20, n_steps), mu_RV_est];
+RV_kappa = [linspace(5, 15, n_steps), kappa_RV_est];
+RV_theta = [linspace(0.05^2, 0.15^2, n_steps), theta_RV_est];
+RV_sigma = [linspace(0.3, 0.8, n_steps), sigma_RV_est];
+RV_rho = [linspace(-0.8, -0.1, n_steps), rho_RV_est];
+
+L_RV_mu = zeros(length(RV_mu),1);
+L_RV_kappa = zeros(length(RV_kappa),1);
+L_RV_theta = zeros(length(RV_theta),1);
+L_RV_sigma = zeros(length(RV_sigma),1);
+L_RV_rho = zeros(length(RV_rho),1);
+
+parfor i=1:length(RV_mu)
+    x1 = x_RV_spx_est;
+    x1(1) = RV_mu(i);
+    L_RV_mu(i,1) = Heston93_nat(x1, simul_real, [spx_ret, spx_RV], 1);
+    
+    x1 = x_RV_spx_est;
+    x1(2) = RV_kappa(i);
+    L_RV_kappa(i,1) = Heston93_nat(x1, simul_real, [spx_ret, spx_RV], 1);
+    
+    x1 = x_RV_spx_est;
+    x1(3) = RV_theta(i);
+    L_RV_theta(i,1) = Heston93_nat(x1, simul_real, [spx_ret, spx_RV], 1);
+    
+    x1 = x_RV_spx_est;
+    x1(4) = RV_sigma(i);
+    L_RV_sigma(i,1) = Heston93_nat(x1, simul_real, [spx_ret, spx_RV], 1);
+    
+    x1 = x_RV_spx_est;
+    x1(5) = RV_rho(i);
+    L_RV_rho(i,1) = Heston93_nat(x1, simul_real, [spx_ret, spx_RV], 1);
+    i
+end
+clear x1 i n_steps
+
+% save likelihood functions
+if save_results==true
+    clear save_results
+    save("real_data_optimization_results_and_likelihood_functions.mat", '-regexp', '^(?!(simul_real)$).')
+end
